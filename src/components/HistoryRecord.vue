045146c38f2bcbbb7bdbcf723e2d0d3772017ba9
志皓 卢 <qq931863282@outlook.com>
2022-07-18 15:20:12 +0800

    历史记录界面和request完成

--- src/components/HistoryRecord.vue
+++ src/components/HistoryRecord.vue
@@ -84,19 +84,25 @@ export default {
                 alert("请输入日期！");
                 return;
             }
-            const data = Net.querySwitchHistoryData('192.168.0.7','01','01',this.queryTime[0],this.queryTime[1]);
-            data===data;
-           // this.historyData = data;
-            this.historyData=   [{ip: '192.168.0.7',address: '02',operaton: '合闸',operator:'user',remark:'无',time:'2022-07-11'}];
-            for(var i = 1;i<=21;i++){
-                this.historyData.push({ip: '192.168.0.7',address: '02',operaton: '合闸',operator:'user',remark:'无',time:'2022-07-11'});
-            }
-            this.total = this.historyData.length;
-            if(this.total<=this.pageSize){
-                this.tableData=this.historyData;
-            }else {
-                this.tableData=(this.historyData).slice(0,this.pageSize);
-            }
+            Net.querySwitchHistoryData('192.168.0.7','01','01',this.queryTime[0],this.queryTime[1])
+                .then((res)=>{
+                    //解析数据
+                     console.log(res.data);
+                    // this.historyData = res.data.data;
+                    // 服务器未建立临时填充数据
+                    this.historyData=   [{ip: '192.168.0.7',address: '02',operaton: '合闸',operator:'user',remark:'无',time:'2022-07-11'}];
+                    for(var i = 1;i<=21;i++){
+                        this.historyData.push({ip: '192.168.0.7',address: '02',operaton: '合闸',operator:'user',remark:'无',time:'2022-07-11'});
+                    }
+                    
+                    this.total = this.historyData.length;
+                    if(this.total<=this.pageSize){
+                        this.tableData=this.historyData;
+                    }else {
+                        this.tableData=(this.historyData).slice(0,this.pageSize);
+                    }
+                }
+           ) 
         },
         reset(){
             this.queryTime='';
--- src/js/request.js
+++ src/js/request.js
@@ -4,86 +4,65 @@ export default{
     test(){
         console.log(host);
     },
-    async closeSwtich(ip, port, address){//分闸
+    async getData(api,data){
         const res = await axios({
-            url:host+'/switch/api/v1/closeSwitch',
+            url:host + api,
             headers:{
                 'Content-Type': 'application/json'
             },
             method:'post',
-            data:JSON.stringify({
-                "address":address,
-                "ip":ip,
-                "port":port
-            })
+            data:data
         })
-        return res.data;
+        return res;
+    }
+    ,
+    closeSwtich(ip, port, address){//分闸
+        const api = '/switch/api/v1/closeSwitch';
+        const data = JSON.stringify({
+            "address":address,
+            "ip":ip,
+            "port":port
+        });
+        return this.getData(api,data);
     },
-    async openSwitch(ip, port, address){//合闸
-        const res = await axios({
-            url:host+'/switch/api/v1/openSwitch',
-            headers:{
-                'Content-Type': 'application/json'
-            },
-            method:'post',
-            data:JSON.stringify({
-                "address":address,
-                "ip":ip,
-                "port":port
-            })
+     openSwitch(ip, port, address){//合闸
+        const api = '/switch/api/v1/openSwitch';
+        const data = JSON.stringify({
+            "address":address,
+            "ip":ip,
+            "port":port
+        });
+        return this.getData(api,data);
+    },
+    queryOrdinarySwitchData(ip, port, address){//查询普通微断数据
+        const api = '/switch/api/v1/queryOrdinarySwitchData';
+        const data = JSON.stringify({
+            "address":address,
+            "ip":ip,
+            "port":port
+        });
+        return this.getData(api,data);
+    },
+    queryThreePhaseSwitchData(ip, port, address){//查询三相微端数据
+        const api = '/switch/api/v1/queryThreePhaseSwitchData';
+        const data = JSON.stringify({
+            "address":address,
+            "ip":ip,
+            "port":port
         })
-        return res.data;
+        return this.getData(api,data)
     },
-    async queryOrdinarySwitchData(ip, port, address){//查询普通微断数据
-        const res = await axios({
-            url:host+'/switch/api/v1/queryOrdinarySwitchData',
-            headers:{
-                'Content-Type': 'application/json'
-            },
-            method:'post',
-            data:JSON.stringify({
-                "address":address,
-                "ip":ip,
-                "port":port
-            })
+    querySwitchHistoryData( ip, port, address, startTime, endTime){//查询历史接口数据
+        const api = '/switch/api/v1/querySwitchHistoryData';
+        const data = JSON.stringify({
+            "address":address,
+            "ip":ip,
+            "port":port,
+            "startTime":startTime,
+            "endTime":endTime
         })
-        return res.data;
-    },
-    async queryThreePhaseSwitchData(ip, port, address){//查询三相微端数据
-        const res = await axios({
-            url:host+'/switch/api/v1/queryThreePhaseSwitchData',
-            headers:{
-                'Content-Type': 'application/json'
-            },
-            method:'post',
-            data:JSON.stringify({
-                "address":address,
-                "ip":ip,
-                "port":port
-            })
-        })
-        return res.data;
-    },
-    async querySwitchHistoryData( ip, port, address, startTime, endTime){//查询历史接口数据
-        await axios({
-            url:host+'/switch/api/v1/queryThreePhaseSwitchData',
-            headers:{
-                'Content-Type': 'application/json'
-            },
-            method:'post',
-            data:JSON.stringify({
-                "address":address,
-                "ip":ip,
-                "port":port,
-                "startTime":startTime,
-                "endTime":endTime
-            })
-        }).then(function(res){
-               // data = res;
-                return res.data;
-            })
-     //   return JSON.parse(obj.response);
-        // return res.data;
+        return this.getData(api,data);
+
     }
     
 
