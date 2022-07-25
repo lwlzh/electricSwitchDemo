b02e2c84b7a3901e97da1063ba56c862783a9f05
志皓 卢 <qq931863282@outlook.com>
2022-07-25 20:23:06 +0800

    历史界面新功能完成，界面美化未完成；新接口本地测试完成，但远程连接存在问题

--- src/components/HistoryRecord.vue
+++ src/components/HistoryRecord.vue
@@ -11,6 +11,7 @@
                 end-placeholder="结束日期"
                 :unlink-panels = "unlinkPanels"
                 :value-format="timeFormat"
+                :picker-options="pickOptions"
                 
             >
             </el-date-picker>
@@ -18,13 +19,13 @@
             <el-button class="btn2" @click="reset">重置</el-button>
          </div>
          <!-- 表格 -->
-        <el-table :data="tableData" style="width: 100%">
+        <el-table :data="tableData" style="width: 100%" :border="tableBorder">
             <!-- 表头 -->
-            <el-table-column prop="ip" label="微断ip" width="180">
+            <el-table-column prop="ip" label="ip" width="180">
             </el-table-column>
             <el-table-column prop="address" label="微端地址" width="180">
             </el-table-column>
-            <el-table-column prop="operaton" label="操作">
+            <el-table-column prop="operation" label="操作">
             </el-table-column>
             <el-table-column prop="operator" label="操作人员">
             </el-table-column>
@@ -55,11 +56,23 @@ export default {
         return{
             queryTime:'',
             timeFormat:"yyyy-MM-dd",
-            unlinkPanels:true,
+            unlinkPanels:false,
             tableData: [],
             nowPage:1,
             total:0,
-            pageSize:20
+            pageSize:20,
+            tableBorder:true,
+            pickOptions:{
+                disabledDate:(time)=>{
+                    var nowDate = new Date(Date.now())
+                  //  console.log(nowDate.prototype.getHours())
+                    nowDate.setHours(0);
+                    nowDate.setMinutes(0);
+                    nowDate.setSeconds(0);
+                    nowDate.setMilliseconds(0);
+                    return time.getTime() > nowDate || time.getTime()<nowDate - 89*24*3600*1000;
+                }
+            }
         }
     },
     methods:{
@@ -68,10 +81,33 @@ export default {
                 alert("请输入日期！");
                 return;
             }
-            Net.querySwitchHistoryData('192.168.0.7','01','01',this.queryTime[0],this.queryTime[1],this.nowPage,this.pageSize)
+            
+            //接口测试代码，后期记得删除
+           /* Net.closeSwtich('192.168.0.7','9999','01').then((res)=>{
+                console.log(res,'close');
+            }).catch(function(e){
+                    console.log(e)
+                })
+            Net.openSwitch('192.168.0.7','9999','01').then((res)=>{
+                console.log(res,'openSwitch');
+            }).catch(function(e){
+                    console.log(e)
+                })
+            Net.queryOrdinarySwitchData('192.168.0.7','9999','01').then((res)=>{
+                console.log(res,'queryOrdinarySwitchData');
+            }).catch(function(e){
+                    console.log(e)
+                })
+            Net.queryThreePhaseSwitchData('192.168.0.7','9999','01').then((res)=>{
+                console.log(res,'queryThreePhaseSwitchData');
+            }).catch(function(e){
+                    console.log(e)
+                })
+            */
+            Net.querySwitchHistoryData('192.168.0.7','9999','01',this.queryTime[0],this.queryTime[1],this.nowPage,this.pageSize)
                 .then((res)=>{
                         //解析数据
-                        console.log(res);
+                       // console.log(res);
                         this.tableData = res.data.list;
                         this.total = res.data.total;
                         // console.log(this.tableData)
